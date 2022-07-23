45382347a24a7cb1cf3d519cc89d700a9f377cbc
Stephanie Aelmore <steph.aelmore@gmail.com>
2022-07-23 10:42:18 -1000

    switched back to adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,16 @@
-import adapter from 'svelte-adapter-static-digitalocean';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-            // default options are shown
-            pages: 'build',
-            assets: 'build',
-            fallback: '404.html',
-            precompress: false,
-            spec: '.do/spec.yaml',
-            name: ''
-        }),
+			// default options are shown
+			out: 'build',
+			precompress: false,
+			envPrefix: ''
+		}),
 		files: {
-			assets: 'static',
+			assets: 'public',
 			lib: 'src/lib',
 			routes: 'src/routes'
 		},
