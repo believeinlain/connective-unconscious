6d2f564ecab9bdfdfb5bde09d711ff41c154a66f
Stephanie Aelmore <steph.aelmore@gmail.com>
2022-07-31 08:55:12 -1000

    don't need hash pwd script anymore

--- hash_pwd.js
+++
@@ -1,9 +0,0 @@
-import * as bcrypt from 'bcrypt';
-
-const args = process.argv.slice(2);
-
-if (args[0]) {
-  const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(args[0], salt);
-  console.log('hashed pwd:', hash);
-}
